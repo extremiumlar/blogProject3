3622598fa399ff52d93efa2f539188a5cf97f3e4
extremiumlar <nurillo1910@gmail.com>
2025-01-11 17:24:38 +0500

    parolni almashtirish qo'shildi
    parolni almashtirish uchun djangoni tayyor viewlaridan foydalanildi . url va templatelar to'g'irlandi

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+)
 from .views import user_login,dashboard_view
 
 urlpatterns = [
@@ -7,4 +12,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', dashboard_view, name='profile'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
